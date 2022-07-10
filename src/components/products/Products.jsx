0c9cdf08fd669690597760f41875d1a1e1fc9257
betty arega <bettyarega3@gmail.com>
2022-07-10 22:58:33 +0300

    products.jsx

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Grid } from '@material-ui/core'
+
+import Product from "./product/Product";
+
+const products = [
+    {id: 1, name: 'Shoes', description: 'Running shoes.', price:'$5', image:'https://hips.hearstapps.com/hmg-prod/images/mens-running-shoes-1650311162.jpg'},
+    {id: 2, name: 'Macbook', description: 'Apple macbook', price:'%6', image:'https://5.imimg.com/data5/SELLER/Default/2021/7/KG/PL/ZG/77832050/apple-mackbook-air-250x250.jpg'},
+]
+
+const Products = () => {
+    return(
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product)=>(
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
+}
+ export default Products
